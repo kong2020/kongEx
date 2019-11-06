bbaec5eb45a29165c1f35ff01215a5d90bdec5fd
kong2020 <alliance140@naver.com>
2019-11-06 11:11:19 +0900

    jpa 레파지토리 메소드 네이밍 규칙 주석추가

--- src/main/java/com/example/todo/repository/TodoListRepository3.java
+++ src/main/java/com/example/todo/repository/TodoListRepository3.java
@@ -7,5 +7,19 @@ import com.example.todo.entity.TodoList;
 
 @Repository
 public interface TodoListRepository3 extends JpaRepository<TodoList, Long> {
-    
+	//(1) findBy로 시작 + Entity 필드 이름
+	//(2) countBy로 시작 + Entity 필드 이름
+	//기본기능을 제외한 조회 기능을 추가하고 싶으면 명명규칙에 맞는 메서드를 추가해야한다.
+	//Member findByName(String name);
+    //Page<Member> findByName(String name, Pageable pageable);
+	
+	//  And:   findByEmailAndUserId(String email, String userId)
+	//  Or:    findByEmailOrUserId(String email, String userId)
+	//  Between: findByCreatedAtBetween(Date fromDate, Date toDate)
+	//  LessThan: findByAgeGraterThanEqual(int age)
+	//  GreaterThanEqual:  findByAgeGraterThanEqual(int age)
+	//  Like:  findByNameLike(String name)
+	//  IsNull: findByJobIsNull()
+	//  In:  findByJob(String … jobs)
+	//  OrderBy: findByEmailOrderByNameAsc(String email)	
 }
